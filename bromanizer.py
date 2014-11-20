8c83c12db5bc054ef33738e9755e5ca25025c7fd
Thura Hlaing <trhura@gmail.com>
2014-11-20 15:19:28 +0630

    initial working commit

--- bromanizer.py
+++ bromanizer.py
@@ -1,17 +1,15 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 from helper import *
+import sys
 
 def find_first_position (string, func):
     for i, c in enumerate(string):
         if func(c): return i
     return -1
 
-def my_phonemic_iter(string):
-    if not isinstance(string, unicode):
-        string = unicode(string, "utf-8")
-
+def myanmar_phonemic_iter(string):
     startpos = 0
     while startpos < len(string):
         curstr = string[startpos:]
@@ -36,14 +34,24 @@ def my_phonemic_iter(string):
         if endpos < len(curstr) and ismyconsonant(curstr[endpos]):
             if endpos + 1 < len(curstr) and curstr[endpos+1] == SIGN_ASAT:
                 endpos += 2
+                while endpos < len(curstr) and ismytone(curstr[endpos]): endpos += 1
+                yield curstr[:endpos]
+                # skip kinzi virama
+                if endpos < len(curstr) and curstr[endpos] == SIGN_VIRAMA: endpos += 1
+                startpos = startpos + endpos
+                continue
+            if endpos + 2 < len(curstr) and curstr[endpos+1] == SIGN_DOT_BELOW and \
+               curstr[endpos+2] == SIGN_ASAT:
+                endpos += 3
+                while endpos < len(curstr) and ismytone(curstr[endpos]): endpos += 1
                 yield curstr[:endpos]
                 startpos = startpos + endpos
                 continue
 
             if endpos + 1 < len(curstr) and curstr[endpos+1] == SIGN_VIRAMA:
                 endpos += 2
-                syll = curstr[:endpos]
-                syll[-1] = SIGN_ASAT # replace virama with asat
+                syll = curstr[:endpos-1] + SIGN_ASAT # replace virama with asat
+                #syll[-1] = SIGN_ASAT
                 yield syll
                 startpos = startpos + endpos
                 continue
@@ -54,5 +62,115 @@ def my_phonemic_iter(string):
 
     raise StopIteration
 
+
+class Romanizer():
+    data = None # subclass needs to override this
+
+    @staticmethod
+    def romanize (cls_type, string):
+        if not issubclass(cls_type, Romanizer):
+            raise TypeError("cls type must be a subclass of Romanizer.")
+        return cls_type.romanize_(string)
+
+    @classmethod
+    def romanize_(cls, string):
+        romans = []
+        maxkeylen = max(len(k) for k in cls.data.keys())
+        for phoneme in myanmar_phonemic_iter(string):
+            romanstr = ""
+            curpos = 0
+            print(phoneme)
+            while curpos < len(phoneme):
+                lookuplen = len(phoneme)
+                while lookuplen > 0:
+                    lookupstr = phoneme[curpos:lookuplen]
+                    #print('\t'+ str(lookuplen)+ phoneme[curpos:lookuplen]+ romanstr)
+                    if lookupstr in cls.data:
+                        romanstr += cls.data[lookupstr]
+                        curpos += (lookuplen - curpos)
+                        break
+                    else:
+                        lookuplen -= 1
+                else:
+                    #sys.stderr.write("Unable to romanize " + phoneme[curpos] + '\n')
+                    romanstr += phoneme[curpos]
+                    curpos += 1
+            romans.append(romanstr)
+
+        return cls.join_(romans)
+
+    @classmethod
+    def join_(cls, romans):
+        return "".join(romans)
+
+import bgn_pcgn
+class BGN_PCGN(Romanizer):
+    data = bgn_pcgn.data
+    vowels = 'aeèioôu'
+
+    @classmethod
+    def join_(cls, romans):
+        new_romans = []
+        for i, roman in enumerate(romans):
+            roman = cls.handle_letter_a (roman)
+            roman = cls.add_vowel_a_if_necessary(roman)
+
+            if roman.startswith('k') and i > 0:
+                if new_romans[i-1][-1] in 'aeioun' or new_romans[i-1].endswith('ng'):
+                    # change ka to ga after vowel sound
+                    roman = 'g' + roman[1:]
+
+            if roman.startswith('s') and i > 0:
+                if new_romans[i-1][-1] in 'aeioun' or new_romans[i-1].endswith('ng'):
+                    # change sa to za after vowel sound
+                    roman = 'z' + roman[1:]
+
+            if roman.startswith('p') and i > 0:
+                if new_romans[i-1][-1] in 'aeioun' or new_romans[i-1].endswith('ng'):
+                    # change pa to ba after vowel sound
+                    roman = 'b' + roman[1:]
+
+            if roman.startswith('t') and not roman.startswith('th') and i > 0:
+                if new_romans[i-1][-1] in 'aeioun' or new_romans[i-1].endswith('ng'):
+                    # change ta to da after vowel sound
+                    roman = 'd' + roman[1:]
+
+            if roman[0] in cls.vowels and i > 0 and not new_romans[i-1][-1].isspace():
+                # add hyphen if started with a vowel
+                roman = '-' + roman
+
+            if roman[0] in 'gy' and i > 0 and new_romans[i-1][-1] == 'n':
+                # to differetiate ng & ny
+                roman = '-' + roman
+
+            if roman[0] == 'h' and i > 0 and new_romans[i-1][-1] == 't':
+                roman = '-' + roman
+
+            new_romans.append(roman)
+
+        #print(new_romans)
+        return "".join(new_romans)
+
+    @classmethod
+    def handle_letter_a(cls, roman):
+        if roman.startswith ('a') and cls.has_vowel(roman[1:]):
+            return roman[1:]
+        return roman
+
+    @classmethod
+    def has_vowel(cls, roman):
+        for v in cls.vowels:
+            #print type(v), type(roman)
+            if roman.find(v) != -1:
+                return True
+        return False
+
+    @classmethod
+    def add_vowel_a_if_necessary(cls, roman):
+        if cls.has_vowel(roman):
+            return roman
+        else:
+            return roman + 'a'
+
 if __name__ == "__main__":
-    main()
+    romanizer = BGN_PCGN_Romainzer()
--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 import random
 import unittest
@@ -14,13 +14,47 @@ class TestSequenceFunctions(unittest.TestCase):
         self.assertEqual (-1, bromanizer.find_first_position("asdf", helper.ismyanmar))
         self.assertEqual (2, bromanizer.find_first_position(u"as\u1000f", helper.ismyconsonant))
 
-    def test_my_phonemic_iter(self):
-        s = "တ|ရား|စီ|ရင်|ရေး|အာ|ဏာ|နှင့်|ဥ|ပ|ဒေ|ပြု|ရေး|အာ|ဏာ|တို့|ကို"
-        ts = "".join(s.split('|'))
-        tr = list(bromanizer.my_phonemic_iter(ts))
-        er = list(unicode(e, 'utf-8') for e in s.split('|'))
-        print ts, tr, er
-        self.assertEqual (tr, er)
+    def test_myanmar_phonemic_iter(self):
+        def test_myanmar_phonemic_iter_(string, ts=None):
+            if not ts: ts = "".join(string.split('|'))
+            tr = list(bromanizer.myanmar_phonemic_iter(ts))
+            er = list(e for e in string.split('|'))
+            self.assertEqual (tr, er)
+
+        test_myanmar_phonemic_iter_("တ|ရား|စီ|ရင်|ရေး|အာ|ဏာ|နှင့်|ဥ|ပ|ဒေ|ပြု|ရေး|အာ|ဏာ|တို့|ကို")
+        test_myanmar_phonemic_iter_("မည်|သူ|မ|ဆို| |ကြည့်|ရှု|ပြင်|ဆင်|နိုင်|သော| |အ|ခ|မဲ့|လွတ်|လပ်|စွယ်|စုံ|ကျမ်း| |ဖြစ်|ပါ|သည်|။")
+        test_myanmar_phonemic_iter_("နာ|နို|တက်|က|နော်|လော်|ဂျီ" , ts="နာနိုတက္ကနော်လော်ဂျီ")
+        test_myanmar_phonemic_iter_("အို|ရှန်း|နီး|ယား")
+
+    def test_bgp_pcgn_romanizer(self):
+        from bromanizer import Romanizer, BGN_PCGN
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, 'ကို'), "ko")
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, 'အက'), "aga")
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, 'မဒမ'), "madama")
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, 'သာငယ္'), 'thangè')
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, 'ပြစင်'), 'pyazin')
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, 'အကာ'), 'aga')
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, 'အိုဘဲ့'), 'obè')
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, "အပ်"), "at")
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, "မအူ"), "ma-u")
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, "သီးပင်အိုင်"),'thibin-aing')
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, "ဩဘာ"), "awba")
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, "ဧဏီ"), "eni")
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, "ကြောင်ဥကျဉ်"), "kyaung-ugyin")
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, "သဒ္ဒါ"), "thadda")
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, "အန္တိမဘဝ"), "andimabawa")
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, "ဥက္ကဌ"), "ukkada")
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, "ရွှေငန်း"),"shwengan")
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, "ညီညာ"), "nyinya")
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, "အင်းကွတ်"),"in-gut")
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, "ကွန်ရက်"),"kun-yet")
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, "တိုင်အောင်"), "taing-aung")
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, "ဟက်ဟက်ပက်ပက်ရယ်"),"het-hetpetpetyè")
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, "ဝသီ"), "wathi")
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, "ဘေးမဲ့"), "bemè")
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, "သင်္ဘော"),"thinbaw")
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, "ဘင်္ဂလားအော်"), "bin-gala-aw")
+        self.assertEqual(Romanizer.romanize(BGN_PCGN, "စင်္ကာပူ"),"sin-gabu")
 
 if __name__ == '__main__':
     unittest.main()
